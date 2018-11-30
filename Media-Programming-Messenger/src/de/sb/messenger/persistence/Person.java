bbc387abaaf853f36dc27c2ef480a9604590c2a5
th30r3tisch <theoretisch@hotmail.de>
2018-11-30 12:54:16 +0100

    removed unused imports

--- Media-Programming-Messenger/src/de/sb/messenger/persistence/Person.java
+++ Media-Programming-Messenger/src/de/sb/messenger/persistence/Person.java
@@ -1,7 +1,6 @@
 package de.sb.messenger.persistence;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 import javax.json.bind.annotation.JsonbProperty;
@@ -11,9 +10,6 @@ import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
