e4268daea42f9c825fffcfa891b54c6fd4084bf7
th30r3tisch <theoretisch@hotmail.de>
2019-02-14 23:43:45 +0100

    refactored test

--- Media-Programming-Messenger-Test/src/de/sb/messenger/persistence/EntityTest.java
+++ Media-Programming-Messenger-Test/src/de/sb/messenger/persistence/EntityTest.java
@@ -108,22 +108,4 @@ public abstract class EntityTest {
 			entityManager.close();
 		}
 	}
-	
-	@Test
-	public void testFindPeople() {
-		
-//		final EntityManager em = getEntityManagerFactory().createEntityManager();
-		
-//		Person p = new Person();
-//		p.getName().setForename("ForeName");
-//		p.getName().setSurname("SurName");
-//		em.getTransaction().begin();
-//		em.persist(p);
-//		em.getTransaction().commit();
-		
-//		Person p2 = em.find(Person.class, p.getIdentity());
-		
-//		assertEquals("V2", p2.getName().getForename(), "Succcesfull");
-		
-	}
 }
---
+++ Media-Programming-Messenger-Test/src/de/sb/messenger/rest/PersonServiceTest.java
@@ -0,0 +1,25 @@
+package de.sb.messenger.rest;
+
+import org.junit.Test;
+
+public class PersonServiceTest extends ServiceTest {
+	
+	@Test
+	public void testFindPeople() {
+		
+//		final EntityManager em = getEntityManagerFactory().createEntityManager();
+		
+//		Person p = new Person();
+//		p.getName().setForename("ForeName");
+//		p.getName().setSurname("SurName");
+//		em.getTransaction().begin();
+//		em.persist(p);
+//		em.getTransaction().commit();
+		
+//		Person p2 = em.find(Person.class, p.getIdentity());
+		
+//		assertEquals("V2", p2.getName().getForename(), "Succcesfull");
+		
+	}
+	
+}
