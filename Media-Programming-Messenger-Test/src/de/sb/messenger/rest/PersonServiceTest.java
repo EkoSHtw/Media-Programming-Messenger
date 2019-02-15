ae2d6f1571f61904710a14398df30eb7dd059127
th30r3tisch <theoretisch@hotmail.de>
2019-02-15 02:22:08 +0100

    test rework

--- Media-Programming-Messenger-Test/src/de/sb/messenger/rest/PersonServiceTest.java
+++ Media-Programming-Messenger-Test/src/de/sb/messenger/rest/PersonServiceTest.java
@@ -1,11 +1,30 @@
 package de.sb.messenger.rest;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.client.WebTarget;
+import org.junit.Assert;
 import org.junit.Test;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class PersonServiceTest extends ServiceTest {
 	
 	@Test
 	public void testFindPeople() {
+		
+		final WebTarget webTarget = newWebTarget("ines.bergmann@web.de", "ines");
+		webTarget.queryParam("resultOffset", "0");
+		webTarget.queryParam("resultLimit", "20");
+		webTarget.queryParam("email", "ines.bergmann@web.de");
+		final String response =
+				webTarget.path("/people")
+	                    .request(APPLICATION_JSON)
+	                    .get(String.class);
+
+//	    final JsonObject result = Json.parse(response).asObject();
+	    
+	    Assert.assertNotEquals(response, null);
+//	    Assert.assertEquals(2, response.size());
 		
 //		final EntityManager em = getEntityManagerFactory().createEntityManager();
 		
